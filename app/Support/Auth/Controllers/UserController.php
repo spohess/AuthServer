d88929f5dd6729e4238ade59c67b96fb6910523f
Sergio Hess <hess.sergio@gmail.com>
2025-08-31 20:08:56 -0300

    feat: implementar coleta de usuários com suporte a filtros e recurso de resposta

--- app/Support/Auth/Controllers/UserController.php
+++ app/Support/Auth/Controllers/UserController.php
@@ -2,13 +2,22 @@
 
 namespace App\Support\Auth\Controllers;
 
+use App\Support\Auth\Actions\Users\UserCollectorAction;
+use App\Support\Auth\Resources\UserResource;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class UserController
 {
-    public function index()
+    public function __construct(
+        private UserCollectorAction $collector,
+    ) {}
+
+    public function index(): AnonymousResourceCollection
     {
-        //
+        return UserResource::collection(
+            $this->collector->collect(),
+        );
     }
 
     public function create()
