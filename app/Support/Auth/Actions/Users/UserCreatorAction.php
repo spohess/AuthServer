06a7d24f1f3e18eba8a8a95dbb9943648812524d
Sergio Hess <hess.sergio@gmail.com>
2025-08-31 14:07:06 -0300

    feat: adicionar ações de criação de perfil e usuário com suporte a UUID

--- app/Support/Auth/Actions/Users/UserCreatorAction.php
+++ app/Support/Auth/Actions/Users/UserCreatorAction.php
@@ -2,18 +2,18 @@
 
 declare(strict_types=1);
 
-namespace App\Support\Auth\Actions;
+namespace App\Support\Auth\Actions\Users;
 
 use App\Base\Interfaces\Actions\CreatorActionInterface;
-use App\Support\Auth\Models\Profile;
+use App\Support\Auth\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
-class ProfileCreatorAction implements CreatorActionInterface
+class UserCreatorAction implements CreatorActionInterface
 {
     public function create(array $args): ?Model
     {
-        return Profile::create([
+        return User::create([
             'id' => Str::uuid()->toString(),
             ...$args,
         ]);
