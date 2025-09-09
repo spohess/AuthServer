7a09b86ec6ad9d9885dd4f08a24d25aaf869ab5d
Sergio Hess <hess.sergio@gmail.com>
2025-09-08 22:15:59 -0300

    feat: atualizar credenciais de autorização e adicionar testes para ações de usuário

---
+++ tests/Feature/Support/Auth/Actions/Users/UserUpdaterActionTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Support\Auth\Actions\Users\UserUpdaterAction;
+use App\Support\Auth\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+beforeEach(function () {
+    $this->updater = app()->make(UserUpdaterAction::class);
+});
+
+describe('Feature teste for UserUpdaterAction', function () {
+    it('should send a new email and update user', function () {
+        $originalEmail = fake()->safeEmail();
+        $newEmail = fake()->safeEmail();
+        $user = User::factory()->create([
+            'email' => $originalEmail,
+        ]);
+
+        $this->updater->update($user, [
+            'email' => $newEmail,
+        ]);
+
+        expect($user->refresh()->email)->toBe($newEmail);
+    });
+
+    it('should update password', function () {
+        $user = User::factory()->create([
+            'password' => bcrypt('TEST'),
+        ]);
+
+        $this->updater->update($user, [
+            'password' => 'password',
+        ]);
+
+        expect(Hash::check('password', $user->refresh()->password))
+            ->toBeTrue();
+    });
+});
