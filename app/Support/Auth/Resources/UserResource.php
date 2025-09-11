c03b4aa485f4d034cc7e113500b373a21226b9eb
Sergio Hess <hess.sergio@gmail.com>
2025-09-10 21:25:08 -0300

    feat: add user creation, retrieval, update, and blocking functionality tests in UserControllerTest

--- app/Support/Auth/Resources/UserResource.php
+++ app/Support/Auth/Resources/UserResource.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace App\Support\Auth\Resources;
 
+use App\Support\Auth\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @mixin User
+ */
 class UserResource extends JsonResource
 {
     public function toArray(Request $request): array
--- tests/Feature/Support/Auth/Controllers/UserControllerTest.php
+++ tests/Feature/Support/Auth/Controllers/UserControllerTest.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 use App\Support\Auth\Controllers\UserController;
 use App\Support\Auth\Models\User;
 use App\Support\Auth\Requests\UserIndexRequest;
+use App\Support\Auth\Requests\UserStoreRequest;
+use App\Support\Auth\Requests\UserUpdateRequest;
+use App\Support\Auth\Resources\UserResource;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 beforeEach(function () {
@@ -65,6 +68,83 @@ describe('Feature test for UserController', function () {
             $resource = $this->controller->index($request);
 
             expect($resource)->toHaveCount(0);
+        });
+    });
+
+    describe('store', function () {
+        it('should create a user', function () {
+            $data = User::factory()->make()->toArray();
+
+            $request = Mockery::mock(UserStoreRequest::class);
+            $request->shouldReceive('validated')
+                ->once()
+                ->andReturn($data);
+            $resource = $this->controller->store($request);
+
+            expect($resource)->toBeInstanceOf(UserResource::class)
+                ->and(json_decode($resource->toJson(), true))->toHaveKeys([
+                    'id',
+                    'email',
+                    'blocked_at',
+                ]);
+            $this->assertDatabaseHas(User::class, $data);
+        });
+    });
+
+    describe('show', function () {
+        it('should get user resource when pass a user', function () {
+            $user = User::factory()->create();
+
+            $resource = $this->controller->show($user);
+
+            expect($resource)->toBeInstanceOf(UserResource::class)
+                ->and(json_decode($resource->toJson(), true))->toHaveKeys([
+                    'id',
+                    'email',
+                    'blocked_at',
+                ]);
+        });
+    });
+
+    describe('update', function () {
+        it('should update user data', function () {
+            $originalEmail = fake()->safeEmail();
+            $newEmail = fake()->safeEmail();
+            $user = User::factory()->create([
+                'email' => $originalEmail,
+            ]);
+
+            $request = Mockery::mock(UserUpdateRequest::class);
+            $request->shouldReceive('validated')
+                ->once()
+                ->andReturn(['email' => $newEmail]);
+
+            $resource = $this->controller->update($request, $user);
+
+            expect($resource)->toBeInstanceOf(UserResource::class)
+                ->and(json_decode($resource->toJson(), true))->toHaveKeys([
+                    'id',
+                    'email',
+                    'blocked_at',
+                ]);
+            $this->assertDatabaseHas(User::class, [
+                'email' => $newEmail,
+            ]);
+        });
+    });
+
+    describe('delete', function () {
+        it('should block user without delete', function () {
+            $user = User::factory()->create([
+                'blocked_at' => null,
+            ]);
+
+            $this->controller->destroy($user);
+
+            $this->assertDatabaseHas(User::class, [
+                'email' => $user->email,
+            ]);
+            expect($user->refresh()->blocked_at)->not()->toBeNull();
         });
     });
 });
